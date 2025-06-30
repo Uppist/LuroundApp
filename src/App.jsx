abf0e4f6970da090f2f5f4917e0b1332d4e6b3eb
David Atat <davidatat8@gmail.com>
2025-06-30 15:13:18 +0100

    Update login

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ export default function App() {
   const [viewerData, setViewerData] = useState(null);
 
   const loginRoutes = [
-    "/LuroundApp",
+    "/",
     "/confirm-password",
     "/Login",
     "/Forgot-password",
@@ -103,7 +103,7 @@ export default function App() {
       {/* All Routes */}
       <Routes>
         {/* Login & Auth Routes */}
-        <Route path="/LuroundApp" element={<CreateAccount />} />
+        <Route path="/" element={<CreateAccount />} />
         <Route path="/confirm-password" element={<Password />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Forgot-password" element={<ForgotPassword />} />
