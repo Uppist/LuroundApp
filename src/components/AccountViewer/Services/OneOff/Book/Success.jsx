9ab358a0bf12e055f869e9ef2dc41bf51e49d4e3
David Atat <davidatat8@gmail.com>
2025-06-30 17:39:56 +0100

    Update sucess icon3

--- src/components/AccountViewer/Services/OneOff/Book/Success.jsx
+++ src/components/AccountViewer/Services/OneOff/Book/Success.jsx
@@ -4,12 +4,13 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import styles from "../style.module.css";
 import { Link } from "react-router-dom";
+import checkIcon from "/public/assets/png/check.png"; // Adjust the path as necessary
 
 export default function Success() {
 
   return (
     <div className={styles.successContainer}>
-      <img src="./public/assets/png/check.png" alt="Success Icon" className={styles.successIcon} />
+      <img src={checkIcon} alt="Success Icon" className={styles.successIcon} />
       <h1 className={styles.successTitle}>Successful</h1>
       <h3>You have successfully booked a Personal Consultation service</h3>
       <Link to='/services'>
