a0e16ed5ce28eb9ddad71c4bc88c747fedc6d3df
David Atat <davidatat8@gmail.com>
2025-06-30 17:34:15 +0100

    Update sucess icon2

--- src/components/AccountViewer/Storefront/CartSuccess.jsx
+++ src/components/AccountViewer/Storefront/CartSuccess.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import styles from './store.module.css';
-import checkIcon from '../icons/check.png'; // Adjust the path as necessary
+import checkIcon from '/public/assets/png/check.png'; // Adjust the path as necessary
 
 export default function CartSuccess({ onBack }) {
   const handleOkayClick = () => {
@@ -11,7 +11,7 @@ export default function CartSuccess({ onBack }) {
 
   return (
     <div className={styles.successContainer}>
-      <img src={checkIcon} alt="Success Icons" className={styles.successIcon} />
+      <img src={checkIcon} alt="Success Icon" className={styles.successIcon} />
       <h1 className={styles.successTitle}>Transaction Successful</h1>
       <button className={styles.successButton} onClick={handleOkayClick}>
         Okay
